d9651a7200851f1db9a71fe5530409622db09d71
Alucard2169 <alucardd2169@gmail.com>
2025-08-24 18:49:47 +0530

    added homepage info button & dialog box

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import Image from 'next/image';
 import 'animate.css';
+import  { useState } from 'react';
 
 
 export default function Home() {
+  const [showTooltip, setShowTooltip] = useState(false);
  return (
 <main className="relative h-screen flex flex-col">
-  <h1 className="absolute font-bold text-3xl inset-0 mx-auto w-fit h-fit top-[20%] 
+  <h1 className="absolute font-bold text-4xl inset-0 mx-auto w-fit h-fit top-[20%] 
                animate__animated animate__fadeInDown">
     ReleaseRadar
   </h1>
@@ -17,6 +20,29 @@ className="border-0 outline-0 font-light bg-[#262626] p-4 rounded-md w-full"/>
           Enter
     </button>
   </section>
+ <section className="absolute bottom-5 right-5">
+        <div className="relative">
+   
+          <p
+            className={`absolute right-10 bottom-0 w-[12rem] p-2 rounded-md bg-gray-800 text-sm origin-bottom-right transform transition-all duration-200 ease-in-out ${
+              showTooltip
+                ? 'opacity-100 scale-100 translate-y-0'
+                : 'opacity-0 scale-75 translate-y-2'
+            }`}
+          >
+            This tool analyzes your project dependencies and highlights outdated packages.
+          </p>
+
+
+          <button
+            onClick={() => setShowTooltip(!showTooltip)}
+            className="rounded-full border w-6 h-6 cursor-pointer hover:bg-gray-400 hover:text-black transition-all ease-linear"
+            title="info"
+          >
+            ?
+          </button>
+        </div>
+      </section>
 </main>
  );
 }
