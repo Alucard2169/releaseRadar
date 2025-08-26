3df998629ce4db829e0518bb88da1ab7dad6001b
Alucard2169 <alucardd2169@gmail.com>
2025-08-26 14:10:51 +0530

    established connection between frontend and backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,75 @@
+# from typing import Union
+# from fastapi import FastAPI
+# import re
+# import httpx
+# app = FastAPI()
+
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+# # parse github url
+# def parse_github_url(url: str) -> Union[dict, None]:
+#     pattern = r"https?://github\.com/(?P<owner>[^/]+)/(?P<repo>[^/]+)(?:/tree/(?P<branch>[^/]+)(?:/(?P<path>.*))?)?"
+#     match = re.match(pattern, url)
+#     if match:
+#         return match.groupdict()
+#     return None
+
+# # fetch github repo content
+# async def fetch_github_repo_content(owner: str, repo: str, branch) -> Dict:
+#     async with httpx.AsyncClient() as client:
+#         base_url = "https://api.github.com"
+#         url = f"{base_url}/repos/{owner}/{repo}/contents?ref={branch}"
+#         response = await client.get(url)
+#         if response.status_code == 200:
+#             return response.json()
+#         else:
+#             return {"error": f"Failed to fetch content: {response.status_code}"}
+        
+
+    
+# @app.get("/fetch")
+# def fetch(url: str):
+#     parsed = parse_github_url(url)
+#     if not parsed:
+#         return {"error": "Invalid GitHub URL"}
+#     owner = parsed["owner"]
+#     repo = parsed["repo"]
+#     branch = parsed.get("branch", "main")
+#     path = parsed.get("path", "")
+#     content = fetch_github_repo_content(owner, repo,branch)
+#     return {"content": content}
+
+
+
+
 from typing import Union
-from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:3000",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/test/")
+def read_test():
+    return {"Test": "Test"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,20 +6,48 @@ import  { useState } from 'react';
 
 export default function Home() {
   const [showTooltip, setShowTooltip] = useState(false);
+  const [repoLink, setRepoLink] = useState("");
+
+  const speakToBackend = async () => {
+    try {
+      const response = await fetch("/api/test");
+      const data = await response.json();
+      console.log(data);
+    }
+    catch(error){
+      console.log("Error communicating with backend:", error);
+    }
+  }
+
+  const handleSubmit = () => {
+    speakToBackend()
+  }
+
+
+
  return (
 <main className="relative h-screen flex flex-col">
   <h1 className="absolute font-bold text-4xl inset-0 mx-auto w-fit h-fit top-[20%] 
                animate__animated animate__fadeInDown">
     ReleaseRadar
   </h1>
+
+
   <section className="m-auto w-1/2 relative">
     <input type="text" placeholder="Paste your repo link here..."
-className="border-0 outline-0 font-light bg-[#262626] p-4 rounded-md w-full"/>
-    <button className="absolute right-2 top-1/2 -translate-y-1/2 w-fit bg-[#454343] p-2 rounded-md cursor-pointer font-light text-sm hover:bg-[#5a5a5a] transition-colors">
+className="border-0 outline-0 font-light bg-[#262626] p-4 rounded-md w-full"
+value={repoLink}
+onChange={(e)=>setRepoLink(e.target.value)}
+required
+/>
+    <button disabled={repoLink.length === 0} onClick={handleSubmit} className="absolute right-2 top-1/2 -translate-y-1/2 w-fit bg-[#454343] p-2 rounded-md cursor-pointer font-light text-sm hover:bg-[#5a5a5a] transition-colors disabled:bg-gray-600 disabled:cursor-not-allowed">
       <Image src="/enterIcon.svg" alt="Enter" width={20} height={20} className="inline-block mr-2"/>
           Enter
     </button>
   </section>
+
+
+
  <section className="absolute bottom-5 right-5">
         <div className="relative">
    
@@ -32,8 +60,6 @@ className="border-0 outline-0 font-light bg-[#262626] p-4 rounded-md w-full"/>
           >
             This tool analyzes your project dependencies and highlights outdated packages.
           </p>
-
-
           <button
             onClick={() => setShowTooltip(!showTooltip)}
             className="rounded-full border w-6 h-6 cursor-pointer hover:bg-gray-400 hover:text-black transition-all ease-linear"
